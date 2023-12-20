99798d8200898bcfc384bacc00dc611510dcf689
nitro <drownlite@gmail.com>
2023-12-20 18:12:15 +0100

    implemented more opcodes

--- src/instructions.nim
+++ src/instructions.nim
@@ -7,7 +7,6 @@
 import std/strutils
 include cpu
 
-
 proc execute (opcode: int) =
     var
         value = reg[opcode and 7]   # Value of the register we are taking a value from 
@@ -20,25 +19,40 @@ proc execute (opcode: int) =
         value = memory[getPair(H, L)]
 
     case opcode
-    of 0x00, 0x10: 
-        # No Operation - NOP
-        # Enter CPU low power mode - STOP
-        discard 
+    of 0x00, 0x10: discard # STOP/NOP
 
     of 0x20, 0x30: 
-        # Relative Jump by i8 if condition is set - JR CC i8
+        # JR CC i8 (Relative Jump by 8 bit integer if condition is set)
         if 
             (opcode == 0x20 and not flags.zero) or 
             (opcode == 0x30 and not flags.carry):
 
             inc cycles
-            pc = nextByte 2
+            inc pc, nextByte() + 2
             
         else:
             inc pc
         
         inc cycles
 
+    of 0xC2, 0xD2, 0xC3, 0xCA, 0xDA, 0xE9:
+        # JP CC n16 (Jump to 16 bit address if condition is set)
+        if 
+            (opcode == 0xC2 and not flags.zero) or
+            (opcode == 0xD2 and not flags.carry) or
+            (opcode == 0xCA and flags.zero) or
+            (opcode == 0xDA and flags.carry) or
+            (opcode == 0xC3):
+                inc cycles, 3
+                pc = uint16(nextByte() + nextByte())
+
+        elif opcode == 0xE9:
+            pc = getPair(H, L)
+
+        else:
+            inc pc, 2
+            inc cycles, 2
+    
     of 0x03, 0x13, 0x23, 0x33, 0x0B, 0x1B, 0x2B, 0x3B: 
         # INC/DEC r16
         inc cycles
@@ -81,7 +95,7 @@ proc execute (opcode: int) =
             false) 
 
     of 0x01, 0x11, 0x21, 0x31: 
-        # Load u16 value into register r16 - LD n16,u16
+        # LD n16,u16 (Load 16 bit unsigned integer into 16 bit register)
         case target
         of B, D, H:
             reg[target] = nextByte()
@@ -92,26 +106,50 @@ proc execute (opcode: int) =
         inc cycles, 2
 
     of 0x02, 0x12, 0x22, 0x32: 
-        # Store value in register A into the byte pointed to by any 16 bit register - LD r16, A
+        # Store value in register A into the byte pointed to by a 16 bit register 
+        inc cycles
+
+        # LD r16, A
         if target == B or target == D:
             memory[getPair(target, target+1)] = value
+            return
 
-        else: # Increment HL afterwards - LD HL+/-, A 
-            memory[getPair(H, L)] = value
+        # LD HL+/-, A
+        memory[getPair(H, L)] = value
 
-            var newValue = getPair(H, L)
-            if target == HL: 
-                inc newValue 
-            else: 
-                dec newValue
+        var newValue = getPair(H, L)
+        if target == HL: 
+            inc newValue 
+        else: 
+            dec newValue
 
-            setPair(newValue, H, L)
+        setPair(newValue, H, L)
 
+    of 0x0A, 0x1A, 0x2A, 0x3A:
+        # Load value from the byte pointed to by a 16 bit register into register A
         inc cycles
 
-    of 0x06, 0x16, 0x26, 0x36: # LD r8, u8
+        if target == B or target == D:
+            reg[A] = memory[getPair(target, target+1)]
+            return
+
+        value = memory[getPair(H, L)]
+
+        if target == HL: 
+            inc value
+        else: 
+            dec value
+        
+        reg[A] = value
+
+    of 0x06, 0x16, 0x26, 0x36: 
+        # LD r8, u8
         inc cycles
-        inc reg[target], nextByte()
+
+        if target == HL:
+            memory[getPair(H, L)] = nextByte()
+        else: 
+            reg[target] = nextByte()
 
     of 0x40..0x7F: 
         # LD r8,r8
@@ -119,14 +157,24 @@ proc execute (opcode: int) =
             memory[getPair(H, L)] = value
         else: 
             reg[target] = value
+        
+    of 0x80..0x8F, 0xC6, 0xD6, 0xE6, 0xF6, 0xCE, 0xDE, 0xEE, 0xFE:
+        var result = case opcode
+        
+        # ADD/ADC A, u8
+        of 0xC6, 0xCE: reg[A] + nextByte()
+        of 0xD6, 0xDE: reg[A] - nextByte()
+        of 0xE6, 0xEE: reg[A] and nextByte()
+        of 0xF6, 0xFE: reg[A] or nextByte()
 
-    of 0x80..0x8F:
         # ADD/ADC A,r8
-        var result = reg[A] + value
+        else: reg[A] + value
 
-                
-        if opcode in 0x88..0x8F:
+        if opcode in 0x88..0x8F or opcode in [0xCE, 0xDE, 0xEE, 0xFE]:
             inc(result, int flags.carry)
+
+        if opcode notin 0x80..0x8F:
+            inc cycles
 
         # https://robdor.com/2016/08/10/gameboy-emulator-half-carry-flag
         flags = (
@@ -134,7 +182,6 @@ proc execute (opcode: int) =
             false, 
             (((value and 0xF) + (reg[A] and 0xF)) and 0x10) == 0x10, 
             result > 255,)
-
             
         reg[A] = result
 
@@ -150,16 +197,15 @@ proc execute (opcode: int) =
             (((value and 0xF) - (reg[A] and 0xF)) and 0x10) == 0x10,
             value > reg[A],)
         
-        # Subtract the value in r8 from A (with carry) - SUB/SBC A,r8
+        # SUB/SBC A,r8 (Subtract the value in register from A )
         if opcode in 0x90..0x9F:
             reg[A] = result
         
-        # Subtract value in r8 from A and set the flags, 
-        # but don't store the result - CP A,r8
+        # CP A,r8 (Subtract and set flags but don't store the result)
         else: discard
 
     of 0xA0..0xB7:  
-        # Bitwise AND/XOR/OR between the value in r8 and A - AND/XOR/OR A,r8
+        # AND/XOR/OR A,r8 (Bitwise operation between the value in register and A)
         reg[A] = case target
         of 4: reg[A] and value
         of 5: reg[A] xor value
@@ -170,22 +216,22 @@ proc execute (opcode: int) =
             reg[A] == 4, false)
 
     of 0xC0, 0xD0, 0xC8, 0xD8, 0xC9:
-        # Return from subroutine  - RET
+        # RET CC (Return from subroutine if condition is set)
         proc ret =
+            inc cycles, 2
             inc stackPointer, 2
             pc = stackPointer
 
         if 
             (opcode == 0xC0 and not flags.zero) or
             (opcode == 0xD0 and not flags.carry) or
-            (opcode == 0xC8 and flags.zero):
+            (opcode == 0xC8 and flags.zero) or
+            (opcode == 0xD8 and flags.carry):
 
                 ret()
-                inc cycles, 3
+                inc cycles
 
-        elif opcode == 0xC9:
-            ret()
-            inc cycles, 2
+        elif opcode == 0xC9: ret()
         
         inc cycles
 
@@ -202,11 +248,11 @@ proc execute (opcode: int) =
         inc cycles, 3
 
     of 0xC7, 0xD7, 0xE7, 0xF7, 0xCF, 0xDF, 0xEF, 0xFF:
-        # Call subroutine - RST
-        dec stackPointer, 2
+        # RST (Call subroutine)
         stackPointer = pc + 1
         inc cycles, 3
-        #pc = case opcode
+
+        pc = uint16 @[0x0, 0x8, 0x10, 0x18, 0x20, 0x28, 0x30, 0x38][target]
     
     else:
         echo toHex opcode, " yet to be implemented"
